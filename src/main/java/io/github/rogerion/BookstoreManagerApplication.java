e46a25ef284be7a66741b4ed78ebc2009461edfb
Rogerio-N <rogenakayama@gmail.com>
2021-07-22 07:42:28 -0400

    [NEW] First commit

---
+++ src/main/java/io/github/rogerion/BookstoreManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rogerion/BookstoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rogerion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
